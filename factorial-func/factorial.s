0fb1a99a87d8b5fff10e07b75f45c3dc27bc11d8
technolocoder <technolocoder@gmail.com>
2021-07-24 14:34:58 +0800

    Fixed Stupid Mistake (I may have been sleepy lol ;])

---
+++ factorial-func/factorial.s
@@ -0,0 +1,37 @@
+.section .text
+.globl _start 
+
+factorial_func:
+    pushq %rbp
+    movq %rsp, %rbp
+    
+    # Check if base case
+    cmpq $1, 16(%rbp) 
+    # If no then callback fibonacci n-1
+    jne factorial_callback
+    # Basecase return "1"
+    movq $1, %rax
+factorial_ret:
+    movq %rbp, %rsp
+    popq %rbp
+    ret
+
+factorial_callback:
+    # Decrement param
+    decq 16(%rbp)
+    pushq 16(%rbp)
+    # Increment for multiplication
+    incq 16(%rbp)
+    call factorial_func
+    imulq 16(%rbp), %rax
+    jmp factorial_ret  
+_start:
+    # Factorial of 5! = 120
+    pushq $5
+    call factorial_func
+    addq $8, %rsp
+
+    # Exit Call
+    movq %rax, %rdi 
+    movq $60, %rax
+    syscall
--- factorial-loop/factorial.s
+++ factorial-loop/factorial.s
@@ -2,15 +2,15 @@
  .globl _start
 
  _start:
-    movq $5, %rdi
+    movq $4, %rdi
     movq $1, %rcx
-    fibonacci_start:
+    factorial_start:
     imulq %rdi, %rcx
     decq %rdi
     cmpq $1, %rdi 
-    je fibonacci_end
-    jmp fibonacci_start
-    fibonacci_end:
+    je factorial_end
+    jmp factorial_start
+    factorial_end:
     movq $60, %rax
     movq %rcx,  %rdi
     syscall
