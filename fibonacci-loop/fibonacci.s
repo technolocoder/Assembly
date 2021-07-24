a3340cde3de39e98876cc3ef73e4b2dc4a8f5823
technolocoder <technolocoder@gmail.com>
2021-07-24 15:01:21 +0800

    Added the real fibonacci (iterative version) ;)

--- fibonacci-loop/fibonacci.s
+++ fibonacci-loop/fibonacci.s
@@ -2,7 +2,38 @@
 .globl _start
 
 _start:
+    movq $8, %rdi
+    
+    cmp $0, %rdi
+    je base_case0 
+    
+    cmp $1, %rdi 
+    je base_case1
+
+    movq $0, %rax 
+    movq $1, %rbx 
+    subq $2, %rdi
+loop_start:
+    movq $0, %rcx 
+    addq %rax, %rcx
+    addq %rbx, %rcx
+
+    cmp $0, %rdi
+    je exit 
+    
+    movq %rbx, %rax
+    movq %rcx, %rbx 
+    decq %rdi
+    jmp loop_start
+
+base_case0:
+    movq $0, %rcx
+    jmp exit
+base_case1: 
+    movq $1, %rcx
+    jmp exit
+exit:
     # Exit Call 
     movq $60, %rax 
-    movq $0 , %rdi
+    movq %rcx , %rdi
     syscall 
