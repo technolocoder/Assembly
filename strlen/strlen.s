5f4538c233ade0206a792f990f282a128c8bac6e
technolocoder <technolocoder@gmail.com>
2021-07-24 21:09:30 +0800

    Modified strlen to only use one register

---
+++ strlen/strlen.s
@@ -0,0 +1,33 @@
+.section .data
+sample_str:
+.ascii "Hello\0"
+
+.section .text 
+.globl _start 
+
+.equ SYS_EXIT, 60
+
+.type strlen,@function
+strlen:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    movq 16(%rbp), %rax
+loop_start:
+    cmpq $0, (%rax)
+    je loop_end # Loop until terminating character is found
+    incq %rax  
+    jmp loop_start 
+
+loop_end:
+    subq 16(%rbp), %rax
+    movq %rbp, %rsp
+    popq %rbp
+    ret
+_start:
+    pushq $sample_str
+    call strlen
+    movq %rax, %rdi
+
+    movq $SYS_EXIT, %rax
+    syscall 
